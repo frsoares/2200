2e8d5a70cb1a0aaed345276a8298da0e1d6abb7e
Eduardo Borges Pinto Osório <ebpo@Eduardo-Osorio-MacBook-Pro.local>
2015-02-03 17:00:43 -0200

    intial commit

---
+++ 2200/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2200
+//
+//  Created by Eduardo Borges Pinto Osório on 2/3/15.
+//  Copyright (c) 2015 Eduardo Borges Pinto Osório. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
