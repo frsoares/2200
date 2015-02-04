20fa04263409b06cd1ce0322d789318ed417b662
Kiev Gama <kievgama@Kievs-MacBook-Pro.local>
2015-02-04 14:16:14 -0200

    new class

---
+++ 2200/GoalStore.swift
@@ -0,0 +1,13 @@
+//
+//  GoalStore.swift
+//  2200
+//
+//  Created by Kiev Gama on 2/4/15.
+//  Copyright (c) 2015 Eduardo Borges Pinto Osório. All rights reserved.
+//
+
+import UIKit
+
+class GoalStore: NSObject {
+   
+}
