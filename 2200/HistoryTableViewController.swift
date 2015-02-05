9deb550d5dc023fbb31a76722834641de31b6103
Francisco Soares <fmssn@cin.ufpe.br>
2015-02-05 15:45:34 -0200

    Loading history in HistoryTableViewController

--- 2200/HistoryTableViewController.swift
+++ 2200/HistoryTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 import HealthKit
 
-var items: [(date: String, steps: String)] = [("27/01/15" , "4500 Steps"), ("28/01/15" , "4500 Steps"), ("29/01/15", "4500 Steps")]
+var items: [(date: String, steps: String)] = [] //[("27/01/15" , "4500 Steps"), ("28/01/15" , "4500 Steps"), ("29/01/15", "4500 Steps")]
 
 
 class HistoryTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -29,8 +29,67 @@ class HistoryTableViewController: UIViewController, UITableViewDelegate, UITable
       
       var ad = UIApplication.sharedApplication().delegate as AppDelegate
       healthStore = ad.healthStore;
+      
+      loadHistoryData();
     }
 
+  func loadHistoryData(){
+    
+    let past = NSDate.distantPast() as NSDate
+    
+    let today = NSDate();
+    
+
+    let calendar = NSCalendar(identifier: NSGregorianCalendar)
+    
+    let teste = NSDateComponents()
+    teste.day=2
+    teste.month=2
+    teste.year=2015
+    teste.hour=0
+    teste.minute=0
+    teste.second=0
+    
+    let anchorDate = calendar?.dateFromComponents(teste);
+    
+    
+    let type : HKQuantityType = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+    
+    let intervalComponents = NSDateComponents();
+    
+    intervalComponents.day = 1;
+    
+    let predicate = HKQuery.predicateForSamplesWithStartDate(past, endDate: today, options: HKQueryOptions.StrictStartDate)
+    
+    let query = HKStatisticsCollectionQuery(quantityType: type, quantitySamplePredicate: predicate, options: HKStatisticsOptions.CumulativeSum, anchorDate: anchorDate, intervalComponents: intervalComponents)
+    
+    query.initialResultsHandler = {(query, results, error) -> Void in
+      
+      if let realResults = results {
+      
+        let statList = realResults.statistics()
+        
+        var unit = HKUnit(fromString: "count");
+        
+        for i : HKStatistics in statList as [HKStatistics] {
+          
+          if let quantity = i.sumQuantity(){
+            let value = Int(quantity.doubleValueForUnit(unit))
+            let day = i.startDate;
+            items += [("\(day)", "\(value)")]
+          }
+          
+        }
+        
+      }
+      
+    }
+    
+    healthStore.executeQuery(query);
+    
+  }
+  
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
